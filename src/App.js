f1c76776fa42cf8ee1a41ecd57b6fc6dba11dba0
Italo Simões <2d.italo@gmail.com>
2023-05-31 14:48:00 -0300

    att para deploy

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 import { Routes, Route } from "react-router-dom";
 import Home from './components/Home/Home';
 import Navbar from './components/Navbar/Navbar';
